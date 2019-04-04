9c67605ae1a3e53256ae32b8df88f59f6765189c
vinnyA3 <vincent.aceto@gmail.com>
2019-04-04 18:06:06 -0400

    feat(utils): create general util file

---
+++ server/utilities/index.js
@@ -0,0 +1,20 @@
+const API_URI = 'https://chumley.barstoolsports.com/dev/data/games';
+
+const requestBuilder = endpoint => ({
+  uri: `${API_URI}/${endpoint}.json`,
+  headers: {
+    'User-Agent': 'Request-Promise',
+  },
+  json: true, // Automatically parses the JSON string in the response
+});
+
+const sportRequestBuilder = type =>
+  ({
+    mlb: requestBuilder('eed38457-db28-4658-ae4f-4d4d38e9e212'),
+    nba: requestBuilder('6c974274-4bfc-4af8-a9c4-8b926637ba74'),
+  }[type] || void 0);
+
+module.exports = {
+  requestBuilder,
+  sportRequestBuilder,
+};
