fbdc41503ad8f5998081ffa86ecacd65d7b6d148
vinnyA3 <vincent.aceto@gmail.com>
2019-04-05 15:22:49 -0400

    feat(client): create app, use react hook for state

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,15 @@
-console.log('helloo world');
+import React, { useEffect, useState } from 'react';
+import { render } from 'react-dom';
+import BoxScore from 'boxScore';
+import getData from 'utils/api';
+
+const App = () => {
+  const [sportData, setSportData] = useState(null);
+  useEffect(() => {
+    getData('/api/sport/mlb').then(({ data }) => setSportData(data));
+  }, []);
+
+  return <>{sportData ? <BoxScore sportData={sportData} /> : ''}</>;
+};
+
+render(<App />, document.getElementById('app'));
