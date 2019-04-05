9692701760f93fee725cd6c6ce04e6a2bfac0614
vinnyA3 <vincent.aceto@gmail.com>
2019-04-05 01:24:19 -0400

    refactor(server): clean up promise chains with async

--- server/routes/controllers/index.js
+++ server/routes/controllers/index.js
@@ -1,15 +1,27 @@
-// const { dbGet } = require('db/connection');
+const { getDb } = require('db/connection');
+const { ObjectID } = require('mongodb');
 const { sportRequestBuilder } = require('utilities');
 const rp = require('request-promise');
 
 module.exports = {
-  getSportData: (req, res) => {
-    const sportRequest = sportRequestBuilder(req.sport);
+  getSportData: async (req, res) => {
+    const sportRequest = sportRequestBuilder(req.sport); // check valid input
 
     if (sportRequest) {
-      rp(sportRequest)
-        .then(data => res.status(200).send({ data }))
-        .catch(err => res.status(500).send({ err }));
+      const db = getDb();
+      const dbData = await db.collection(req.sport).findOne({});
+
+      if (!dbData) {
+        const data = await rp(sportRequest);
+        await db.collection(req.sport).insertOne(data);
+        res.status(200).send(data);
+      } else {
+        // get the timestamp of the data ...
+        // if the timestamp is < 15 seconds from the currentTime, return that
+        // 'cached' data
+        const dataTimestamp = dbData._id.getTimestamp();
+        res.status(200).send(dbData);
+      }
     } else {
       res
         .status(400)
