c0e4460877eb33da808ee57004c24b426e6d3b7a
vinnyA3 <vincent.aceto@gmail.com>
2019-04-04 20:20:05 -0400

    feat(config): rename export obj props

--- server/config/index.js
+++ server/config/index.js
@@ -1,13 +1,13 @@
 const env = process.env.NODE_ENV;
 
 const dev = {
-  app: {
+  web: {
     port: parseInt(process.env.DEV_APP_PORT || 3000),
   },
   db: {
     host: process.env.DEV_DB_HOST || 'localhost',
-    port: parseInt(process.env.DEV_DB_PORT) || 27107, // default mongo
-    name: process.env.DEV_DB_NAME || 'database',
+    port: parseInt(process.env.DEV_DB_PORT) || 27017, // default mongo
+    name: process.env.DEV_DB_NAME || 'barstool-fullstack',
   },
 };
 
