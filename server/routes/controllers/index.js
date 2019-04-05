bcb96ea411ba9bc3d31974b7e158e6fa9479e52b
vinnyA3 <vincent.aceto@gmail.com>
2019-04-05 02:38:08 -0400

    feat(controllers): implement mongodb 'cache'

--- server/routes/controllers/index.js
+++ server/routes/controllers/index.js
@@ -5,27 +5,35 @@ const rp = require('request-promise');
 
 module.exports = {
   getSportData: async (req, res) => {
-    const sportRequest = sportRequestBuilder(req.sport); // check valid input
+    const sportRequest = sportRequestBuilder(req.sport); // check valid input & build req
 
     if (sportRequest) {
-      const db = getDb();
-      const dbData = await db.collection(req.sport).findOne({});
+      try {
+        const db = getDb();
+        const dbData = await db.collection(req.sport).findOne({});
 
-      if (!dbData) {
-        const data = await rp(sportRequest);
-        await db.collection(req.sport).insertOne(data);
-        res.status(200).send(data);
-      } else {
-        // get the timestamp of the data ...
-        // if the timestamp is < 15 seconds from the currentTime, return that
-        // 'cached' data
-        const dataTimestamp = dbData._id.getTimestamp();
-        res.status(200).send(dbData);
+        if (!dbData) {
+          const data = await rp(sportRequest);
+          db.collection(req.sport).insertOne(data); // dont' await the data ...
+          res.status(200).send(data);
+        } else {
+          const now = Date.now();
+          const timestamp = dbData._id.getTimestamp();
+
+          if (Math.floor((now - timestamp) / 1000) < 15) {
+            res.status(200).send(dbData);
+          } else {
+            const data = await rp(sportRequest);
+            await db.collection(req.sport).drop();
+            db.collection(req.sport).insertOne(data); // dont' await the data ...
+            res.status(200).send(data);
+          }
+        }
+      } catch (err) {
+        res.status(500).send({ err });
       }
     } else {
-      res
-        .status(400)
-        .send({ error: 'Please supply a valid sport/league ie. MLB, NBA' });
+      res.status(400).send({ err: 'Invalid league' });
     }
   },
 };
