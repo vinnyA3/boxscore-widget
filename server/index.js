914a658c12f18335386774f0aed5a6147e3642cd
vinnyA3 <vincent.aceto@gmail.com>
2019-04-04 20:20:41 -0400

    refactor(server): use db connection & wrap app

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const port = 3000;
+const { web, db } = require('./config');
+const { dbConnect } = require('db/connection');
+const port = web.port;
+// INIT
 const app = express();
-const apiRouter = require('./routes');
 
 // MIDDLEWARE
 app.use(morgan('dev'));
@@ -11,10 +13,14 @@ app.use(express.json());
 app.use(cors());
 
 // ROUTES
-app.use('/api', apiRouter);
+app.use('/api', require('./routes'));
 
 app.get('*', (req, res) =>
   res.status(404).send({ message: 'Page not found.' })
 );
 
-app.listen(port, () => (console.log(`Listening on port: ${port}`), void 0));
+dbConnect(`mongodb://${db.host}:${db.port}`, db.name)
+  .then(() =>
+    app.listen(port, () => (console.log(`Listening on port: ${port}`), void 0))
+  )
+  .catch(err => (console.error(err), process.exit(1)));
