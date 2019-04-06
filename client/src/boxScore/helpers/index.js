d7f2ae1ca0def8b5f8ed84b36057910537a7ec1c
vinnyA3 <vincent.aceto@gmail.com>
2019-04-05 23:31:56 -0400

    refactor(helpers): DRY + dynamicity
    Add dynamicity to helpers (account for different leagues + periods)

--- client/src/boxScore/helpers/index.js
+++ client/src/boxScore/helpers/index.js
@@ -1,54 +1,65 @@
 import React from 'react';
 
-const calcTotalScore = scores =>
-  scores.reduce((total, score) => (total += score));
-
-const getNumOfInnings = arr => (arr.length < 9 ? 9 : arr.length);
-
-export const genLinescoreHeaderItems = sportData => {
-  const statHeaders = ['R', 'H', 'E'];
-  const numInnings = getNumOfInnings(sportData.away_period_scores);
-  const linescoreItems = [];
-
-  for (let i = 1; i <= numInnings; i++) {
-    linescoreItems.push(<td key={i}>{i}</td>);
-  }
-
-  statHeaders.forEach(header =>
-    linescoreItems.push(
-      <td key={header}>
-        <strong>{header}</strong>
+const genMLBData = (data, arr) =>
+  data.forEach(val =>
+    arr.push(
+      <td key={val}>
+        <strong>{val}</strong>
       </td>
     )
   );
+
+const calcTotalScore = scores =>
+  scores.reduce((total, score) => (total += score));
+
+const getNumOfCells = league =>
+  ({
+    MLB: arr => (arr.length < 9 ? 9 : arr.length),
+    NBA: arr => (arr.length < 4 ? 4 : arr.length),
+  }[league]);
+
+export const genLinescoreHeaderItems = sportData => {
+  const linescoreItems = [];
+  const numCells = getNumOfCells(sportData.league)(
+    sportData.away_period_scores
+  );
+
+  for (let i = 1; i <= numCells; i++) {
+    linescoreItems.push(<td key={i}>{i}</td>);
+  }
+
+  if (sportData.league === 'MLB') {
+    genMLBData(['R', 'H', 'E'], linescoreItems);
+  }
 
   return linescoreItems;
 };
 
 export const genLinescoreItems = (sportData, team) => {
   const linescoreItems = [];
-  const numInnings = getNumOfInnings(sportData.away_period_scores);
+  const numCells = getNumOfCells(sportData.league)(
+    sportData.away_period_scores
+  );
+
   const teamScores = sportData[`${team}_period_scores`];
   const totalTeamScore = calcTotalScore(teamScores);
-  const teamStats = [
-    totalTeamScore,
-    sportData[`${team}_batter_totals`].hits,
-    sportData[`${team}_errors`],
-  ];
 
-  for (let i = 0; i < numInnings; i++) {
+  for (let i = 0; i < numCells; i++) {
     linescoreItems.push(
       <td>{teamScores[i] !== undefined ? teamScores[i] : '-'}</td>
     );
   }
 
-  teamStats.forEach(stat =>
-    linescoreItems.push(
-      <td>
-        <strong>{stat}</strong>
-      </td>
-    )
-  );
+  if (sportData.league === 'MLB') {
+    genMLBData(
+      [
+        totalTeamScore,
+        sportData[`${team}_batter_totals`].hits,
+        sportData[`${team}_errors`],
+      ],
+      linescoreItems
+    );
+  }
 
   return linescoreItems;
 };
