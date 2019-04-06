ba031f1386319a6237d60a34135d390082d48b03
vinnyA3 <vincent.aceto@gmail.com>
2019-04-05 23:29:37 -0400

    feat(index): modify effect, add nav component
    Modified the effect hook to allow for data updates

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,38 @@ import React, { useEffect, useState } from 'react';
 import { render } from 'react-dom';
 import BoxScore from 'boxScore';
 import getData from 'utils/api';
+import './sass/main';
 
 const App = () => {
   const [sportData, setSportData] = useState(null);
-  useEffect(() => {
-    getData('/api/sport/mlb').then(({ data }) => setSportData(data));
-  }, []);
+  const [currentLeague, setLeague] = useState('mlb');
 
-  return <>{sportData ? <BoxScore sportData={sportData} /> : ''}</>;
+  useEffect(() => {
+    getData(`/api/sport/${currentLeague}`).then(({ data }) =>
+      setSportData(data)
+    );
+  }, [currentLeague]);
+
+  return sportData ? (
+    <>
+      <nav>
+        <aside>
+          <h1>Barstool</h1>
+        </aside>
+        <ul>
+          <li>
+            <span onClick={() => setLeague('mlb')}>MLB</span>
+          </li>
+          <li>
+            <span onClick={() => setLeague('nba')}>NBA</span>
+          </li>
+        </ul>
+      </nav>
+      <BoxScore sportData={sportData} />
+    </>
+  ) : (
+    ''
+  );
 };
 
 render(<App />, document.getElementById('app'));
