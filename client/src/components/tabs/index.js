ec4ecc4ee8dedea505e2b6a10962af90ac6acf90
vinnyA3 <vincent.aceto@gmail.com>
2019-04-08 17:44:58 -0400

    feat(tabs): create reusable component

--- client/src/components/tabs/index.js
+++ client/src/components/tabs/index.js
@@ -1,14 +1,31 @@
 import React, { useState } from 'react';
+import {
+  tabContainer,
+  tabContainerInline,
+  tab,
+  tabInline,
+  active,
+} from './styles';
 
 export const Tabs = props => {
   const [activeTabIndex, setActiveTab] = useState(0);
+  const onClick = (i, title) =>
+    props.run ? (props.run(title), setActiveTab(i)) : setActiveTab(i);
 
   return (
-    <ul>
+    <ul
+      className={`${tabContainer} ${!!props.inline ? tabContainerInline : ''}`}
+    >
       {props.children.map((el, i) => (
-        <span key={i} onClick={() => setActiveTab(i)}>
+        <li
+          className={`${tab} ${!!props.inline ? tabInline : ''} ${
+            activeTabIndex === i ? active : ''
+          }`}
+          key={i}
+          onClick={() => onClick(i, el.props.title)}
+        >
           {el.props.title}
-        </span>
+        </li>
       ))}
     </ul>
   );
