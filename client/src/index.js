f03aeb4a5df6989cb1793080b7170adcef215770
vinnyA3 <vincent.aceto@gmail.com>
2019-04-08 18:06:19 -0400

    feat(client): import helpers in index

--- client/src/index.js
+++ client/src/index.js
@@ -3,17 +3,19 @@ import { render } from 'react-dom';
 import { Nav, NavBrand } from 'components/Navigation';
 import { Tabs } from 'components/tabs';
 import BoxScore from 'BoxScore';
+import { compose2, toLower } from 'utils/helpers';
 import getData from 'utils/api';
 import './sass/main';
 
 const App = () => {
   const [sportData, setSportData] = useState(null);
   const [currentLeague, setLeague] = useState('mlb');
+  const lowerAndSetLeague = compose2(setLeague, toLower);
 
   useEffect(() => {
-    getData(`/api/sport/${currentLeague}`).then(({ data }) =>
-      setSportData(data)
-    );
+    getData(`/api/league/${currentLeague}`)
+      .then(({ data }) => setSportData(data))
+      .catch(e => console.error(e));
   }, [currentLeague]);
 
   return sportData ? (
@@ -22,9 +24,9 @@ const App = () => {
         <NavBrand>
           <h1>Barstool</h1>
         </NavBrand>
-        <Tabs inline={true} run={setLeague}>
-          <li title="mlb" />
-          <li title="nba" />
+        <Tabs inline={true} run={lowerAndSetLeague}>
+          <li title="MLB" />
+          <li title="NBA" />
         </Tabs>
       </Nav>
       <main
